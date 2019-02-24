dc586dc9a4eda0a82595f452720bfbb8ee7430f4
Dave Peckham <dave@icoracing.com>
2019-02-24 14:27:54 -0800

    renaming

--- cross-country/validate_schemas.sh
+++ cross-country/validate_schemas.sh
@@ -1,3 +1,2 @@
 xmllint --noout --schema http://www.w3.org/2001/XMLSchema.xsd openrally.xsd
 xmllint --noout --schema http://www.w3.org/2001/XMLSchema.xsd ../external/gpx.xsd
-xmllint --noout --schema test_wrapper.xsd example.gpx
